e3a8c387d667c5fa9b2c7d140f3f8ede22edbe7f
Denise Torres <dentorres@hotmail.com>
2022-06-20 21:14:20 -0400

    se agregan imagenes y eventos

--- script2.js
+++ script2.js
@@ -1,18 +1,19 @@
 //Carrito
 
 class Producto {
-    constructor(nombre, precio){
+    constructor(nombre, precio, imagen){
         this.nombre = nombre;
         this.precio = precio;
+        this.imagen = imagen;
     }
 }
 
-const producto1 = new Producto ("Cubrebidones grande", 2000)
-const producto2 = new Producto ("Cubrebidones chico", 1500)
-const producto3 = new Producto ("Espejo grande", 2500)
-const producto4 = new Producto ("Espejo chico", 2100)
-const producto5 = new Producto ("Portamacetas", 2050)
-const producto6 = new Producto ("Adornos", 1200)
+const producto1 = new Producto ("Tapiz grande", 8000, "tapizGrande.png")
+const producto2 = new Producto ("Tapiz chico", 4500, "tapizChico.png")
+const producto3 = new Producto ("Espejo grande", 2500, "espejoGrande.png")
+const producto4 = new Producto ("Espejo chico", 2100, "espejoChico.png")
+const producto5 = new Producto ("Portamacetas", 2050, "portamacetas.png")
+const producto6 = new Producto ("Adornos", 1200, "adornos.png")
 
 let productos = [producto1, producto2, producto3, producto4, producto5, producto6]
 let carrito = []
@@ -23,21 +24,21 @@ if(localStorage.getItem("carrito")) {
     localStorage.setItem("carrito", JSON.stringify(carrito))
 }
 
-
 let divProductos = document.querySelector("#divProductos")
 productos.forEach((productoEnArray, indice) => {
     divProductos.innerHTML += `
-    <div class="card border-primary mb-3 row col-md-3 border-start border-dark margin-3" id="producto${indice}">
-        <img src="./img/${productoEnArray.imagenes}" class="card-img-top" alt="foto1">
+    <div class="card border-secondary row col-md-3 margin-3" id="producto${indice}">
+        <h5 class="card-title">${productoEnArray.nombre}</h5>
         <div class="card-body">
-          <h5 class="card-title">${productoEnArray.nombre}</h5>
+          <img src="./imagenes/${productoEnArray.imagen}" class="card-img-top" alt="foto1">
           <p class="card-text">$${productoEnArray.precio}</p>
           <button id="boton${indice}">Comprar</button>
         </div>
     </div>
-</div>
     `
 })
+
+
 
 productos.forEach((productoEnArray, indice)=> {
     document.querySelector(`#boton${indice}`).addEventListener("click", ()=> {
@@ -48,6 +49,7 @@ productos.forEach((productoEnArray, indice)=> {
     })
 })
 
+// Button para agregar producto al carrito con alerta de Toastify
 let botonCarrito = document.querySelector("#divProductos")
 botonCarrito.addEventListener("click", () => {
     Toastify({
@@ -64,4 +66,18 @@ botonCarrito.addEventListener("click", () => {
         },
         onClick: function(){} // Callback after click
       }).showToast();
+
+
 })
+let buttonCarrito = document.querySelector("#buttonCarrito")
+buttonCarrito.addEventListener("click", () =>{
+    Swal.fire({
+        title: 'Sweet!',
+        text: 'Modal with a custom image.',
+        imageUrl: 'https://unsplash.it/400/200',
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'Custom image',
+      })
+})
+
